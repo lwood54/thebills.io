78129ee068dd40aef9e187d3dac11357f5c84796
Logan Wood <lwood3499@gmail.com>
2020-12-21 17:32:54 -0600

    first commit

---
+++ src/main/java/io/thebills/billtracking/BilltrackingApplication.java
@@ -0,0 +1,13 @@
+package io.thebills.billtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilltrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilltrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/thebills/billtracking/BilltrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.thebills.billtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilltrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
