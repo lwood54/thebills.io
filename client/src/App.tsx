6f179244240a7d054dd0fd7b9a65701c9cc6e668
Logan Wood <lwood3499@gmail.com>
2020-12-24 11:50:50 -0600

    initialize FE

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FirstComponent from "./components/user/FirstComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Bills IO</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/user/FirstComponent.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+function FirstComponent() {
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFormInput(e: React.FormEvent<HTMLInputElement>) {
+    const name = e.currentTarget.name;
+    const val = e.currentTarget.value;
+    switch (name) {
+      case "email":
+        setEmail(val);
+        break;
+      case "firstName":
+        setFirstName(val);
+        break;
+      case "lastName":
+        setLastName(val);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const user = {
+      email,
+      firstName,
+      lastName,
+    };
+
+    console.log("submitting", user);
+  }
+  return (
+    <div>
+      <h1>Add a user</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email
+          <input type="text" name="email" onChange={handleFormInput} />
+        </label>
+        <label htmlFor="firstName">
+          First Name
+          <input type="text" name="firstName" onChange={handleFormInput} />
+        </label>
+        <label htmlFor="lastName">
+          Last Name
+          <input type="text" name="lastName" onChange={handleFormInput} />
+        </label>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default FirstComponent;
